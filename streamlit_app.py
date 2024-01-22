c1aef089b8537fbd940b7d107f3a25b746abef37
i-LUDUS <dietrich@heiser.be>
2024-01-22 18:57:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import shutil
 
 def save_uploaded_file(uploaded_file):
     try:
@@ -9,8 +10,16 @@ def save_uploaded_file(uploaded_file):
     except:
         return False
 
+def list_files(directory):
+    files = []
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
 def main():
-    st.title('Video Upload App')
+    st.title('Video Upload en Beheer App')
 
     # Maak een directory als deze nog niet bestaat
     if not os.path.exists('uploaded_videos'):
@@ -24,6 +33,20 @@ def main():
             st.success('Video succesvol geüpload.')
         else:
             st.error('Fout bij het uploaden van de video.')
+
+    # Lijst van geüploade bestanden
+    st.subheader("Beschikbare Video's")
+    for filename in list_files("uploaded_videos"):
+        with st.container():
+            st.write(filename)
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                if st.button(f'Afspelen {filename}'):
+                    st.video(os.path.join("uploaded_videos", filename))
+            with col2:
+                if st.button(f'Verwijderen {filename}'):
+                    os.remove(os.path.join("uploaded_videos", filename))
+                    st.experimental_rerun()
 
 if __name__ == "__main__":
     main()
